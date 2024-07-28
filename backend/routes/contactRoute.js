dfe5252348459cc02ee9b0ec2df671d052ac5fd7
Vatanpreetkaur <Vatanpreet914@gmail.com>
2024-07-27 23:38:12 -0700

    contact api

---
+++ backend/routes/contactRoute.js
@@ -0,0 +1,9 @@
+// routes/contactRoutes.js
+const express = require("express");
+const router = express.Router();
+const { createContact } = require("../controller/contactController");
+
+
+router.post("/", createContact);
+
+module.exports = router;
